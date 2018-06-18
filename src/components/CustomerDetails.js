65a2d2d2a936541899969f669fa0f8e75ec42d6f
Kaitlin Forsman <kcforsman@outlook.com>
2018-06-18 16:41:18 -0700

    finished implementing logic for displaying basic customer details to the CustomerList

--- src/components/CustomerDetails.js
+++ src/components/CustomerDetails.js
@@ -5,11 +5,27 @@ import axios from 'axios';
 import './CustomerDetails.css';
 
 class CustomerDetails extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    credit: PropTypes.number.isRequired,
+  }
+
+  displayCredit = () => {
+    if (this.props.credit.length == 0) {
+      return <dd><strong>Account Credit:</strong> {this.props.credit}</dd>
+    }
+
+  }
+
   render() {
 
     return (
-      <section>
-      </section>
+      <dl>
+        <dt><strong>Name:</strong> { this.props.name }</dt>
+        <dd><strong>Phone Number:</strong> {this.props.phone}</dd>
+        { this.displayCredit() }
+      </dl>
     )
   }
 
