6e9e61c2fc2421a5d260bdcb380aa26cf5592132
Kaitlin Forsman <kcforsman@outlook.com>
2018-06-18 16:26:25 -0700

    added logic to CustomerList to make api call to retrieve list of customers from our Rails api, and installed axios to package.json

---
+++ src/components/Customer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+import './Customer.css';
+import CustomerDetails from './CustomerDetails';
+
+class Customer extends Component {
+  render() {
+
+    return (
+      <section>
+      </section>
+    )
+  }
+
+}
+
+
+export default Customer;
---
+++ src/components/CustomerDetails.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+import './CustomerDetails.css';
+
+class CustomerDetails extends Component {
+  render() {
+
+    return (
+      <section>
+      </section>
+    )
+  }
+
+}
+
+
+export default CustomerDetails;
